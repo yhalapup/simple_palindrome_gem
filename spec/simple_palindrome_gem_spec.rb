5db174c806ee1842d9ac1b887bb96b2f3de08bbb
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-14 12:40:08 +0300

    add negative test

--- spec/simple_palindrome_gem_spec.rb
+++ spec/simple_palindrome_gem_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe SimplePalindromeGem do
   it "has a version number" do
     expect(SimplePalindromeGem::VERSION).not_to be nil
   end
+
+  it "returns false for non-palindrome" do
+    expect("apple".palindrome?).to eq(false)
+  end
 end
