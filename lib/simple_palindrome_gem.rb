ea67273b2cb7de9a67453d5e2c43a1efb93a60cb
Yurii Halapup <halapup.yurii@gmail.com>
2018-10-15 11:36:56 +0300

    add letters method implementation so tests now pass

--- lib/simple_palindrome_gem.rb
+++ lib/simple_palindrome_gem.rb
@@ -7,6 +7,17 @@ class String
     processed_content == processed_content.reverse
   end
 
+  # Returns the letters in the string.
+  def letters
+    the_letters = []
+    for i in 0..self.length-1 do
+      if self[i].match(/[a-zA-Z]/)
+        the_letters << self[i]
+      end
+    end
+    the_letters.join
+  end
+
   private
 
   # Returns content for palindrome testing.
